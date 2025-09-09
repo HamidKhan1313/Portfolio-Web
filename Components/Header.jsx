9e57c854525815983fc9b36682ba00b40b9dce64
HamidKhan1313 <hamidkhantanol@gmail.com>
2025-09-09 22:42:39 +0500

    changes

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,38 +1,67 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
 
 const Header = () => {
     return (
         <div className='w-11/12 max-w-3x1 text-center mx-auto h-screen flex flex-col items-center justify-center
-        gap-4
-        '>
-            <div>
-                <Image src={assets.profile_img} alt=''  className='rounded-full w-40  mt-40  mr-20' />
-            </div>
-            <h3 className='flex items-end gap-5 text-xl md:text-3xl  mb-3  font-Ovo' >
-                Hi! I'm Hamid Khan   <Image src={assets.hand_icon} alt='' className=' w-10' /></h3>
+        gap-4'>
+            <motion.div
+                initial={{ scale: 0 }}
+                whileInView={{ scale: 1 }}
+                transition={{ duration: 0.8, type: 'spring', stiffness: 100 }}
+            >
+                <Image src={assets.profile_img} alt='' className='rounded-full w-40  mt-40  mr-20' />
+            </motion.div>
+            < motion.h3
+                initial={{ y: -20, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.6, delay: 0.3 }}
+                className='flex items-end gap-5 text-xl md:text-3xl  mb-3  font-Ovo' >
+                Hi! I'm Hamid Khan   <Image src={assets.hand_icon} alt='' className=' w-10' />
+            </motion.h3>
 
-            <h1 className='text-3x1 sm:text-6xl lg:text-[68px] mb-2 font-ovo'>
+            <motion.h1
+                initial={{ y: -30, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.8, delay: 0.5 }}
+
+                className='text-3x1 sm:text-6xl lg:text-[68px] mb-2 font-ovo'>
                 Frontend web developer based in Pakistan
-            </h1>
-            <p className='max-w-3xl mx-auto font-Ovo '>
-            I am a frontend developer from Abbottabad, Pakistan with 2 month of Intership experence in a 
-            company DevDiligent.
-            </p>
+            </motion.h1>
+            <motion.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 0.6, delay: 0.7 }}
+
+                className='max-w-3xl mx-auto font-Ovo '>
+                I am a frontend developer from Abbottabad, Pakistan with 2 month of Intership experence in a
+                company DevDiligent.
+            </motion.p>
             <div className=' w-full max-w-3xl text-center mx-auto h-screen flex flex-col
             items-center justify-center gap-4'>
-          <a href=" #contact"
-           className='px-6 py-3   border border-white rounded-full bg-black
-           text-white  flex items-center gap-2  mr-15'
-            >contact me  <Image src={assets.right_arrow_white}
-          alt='' className='w-4 ' /></a>
+                <motion.a
+                    initial={{ y: 30, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 0.6, delay: 1 }}
 
-            <a href="/sample-resume.pdf" download 
-            className='px-6 py-3   border rounded-full border-gray-500 flex 
+                    href=" #contact"
+                    className='px-6 py-3   border border-white rounded-full bg-black
+           text-white  flex items-center gap-2  mr-15'
+                >contact me  <Image src={assets.right_arrow_white}
+                    alt='' className='w-4 ' /></motion.a>
+
+                <motion.a
+                    initial={{ y: 30, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 0.6, delay: 1.2 }}
+
+                    href="/sample-resume.pdf" download
+                    className='px-6 py-3   border rounded-full border-gray-500 flex 
             items-center gap-5 mr-15  '>
-             my resume <Image src={assets.download_icon}
-          alt='' className='w-4' /></a>
+                    my resume <Image src={assets.download_icon}
+                        alt='' className='w-4' /></motion.a>
 
             </div>
         </div>
--- Components/Services.jsx
+++ Components/Services.jsx
@@ -1,43 +1,66 @@
 import { assets, serviceData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
 
 const Services = () => {
     return (
-        <div id='Services'
-            className='w-full px-[12%] py-10 scroll-mt-5 mb-18 '>
-            <h4 className='text-center  text-lg font-Ovo mt-48  ml-20' >
-                What I offer
-            </h4>
-            <h2 className='text-center text-5xl font-Ovo mt-3 ml-15'>
+        <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            id='Services' className='w-full px-[12%] py-10 scroll-mt-20 '>
+
+            <motion.h4
+                initial={{ y: -20, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{ delay: 0.3, duration: 0.5 }}
+                className='text-center mb-2 text-lg font-Ovo ' >
+                What I offer</motion.h4>
+            <motion.h2
+                initial={{ y: -20, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{ delay: 0.5, duration: 0.5 }}
+                className='text-center text-5xl font-Ovo'>
                 My Services
-            </h2>
-            <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+            </motion.h2>
+            <motion.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ delay: 0.7, duration: 0.5 }}
+                className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
 
                 I am a frontend developer from Abbottabad, Pakistan with 2 month experence in company DevDiligent.
-               </p>
-                <div className='grid grid-cols-4 gap-6 my-10'>
+            </motion.p>
+            <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ delay: 0.9, duration: 0.6 }}
+                className='grid grid-cols-4 gap-6 my-10'>
 
-                    {serviceData.map(({icon, title, description, link}, index) => (
-                        <div key={index}
+                {serviceData.map(({ icon, title, description, link }, index) => (
+                    <motion.div
+                       whileHover={{scale: 1.05}}
+                        key={index}
                         className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black 
-                        cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500'>
-                            <Image src={icon} alt='' className='w-10' />
-                            <h3 className='text-lg my-4 text-gray-700'>{title}</h3>
-                            <p className='text-sm text-gray-600 leading-5'>
-                                {description}
-                            </p>
-                            <a href={link} className='flex items-center gap-2 text-sm mt-5'>
-                                Read more  <Image alt='' src={assets.right_arrow} className='w-4' />
-                            </a>
-                        </div>
+                        cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover
+                        dark:hover:shadow-white'>
+                        <Image src={icon} alt='' className='w-10' />
+                        <h3 className='text-lg my-4 text-gray-700 dark:text-white'>{title}</h3>
+                        <p className='text-sm text-gray-600 leading-5 dark:text-white/80'>
+                            {description}
+                        </p>
+                        <a href={link} className='flex items-center gap-2 text-sm mt-5'>
+                            Read more  <Image alt='' src={assets.right_arrow} className='w-4' />
+                        </a>
+                    </motion.div>
 
-                    ))}
+                ))}
 
-                </div>
+            </motion.div>
 
-         
-        </div>
+
+        </motion.div>
     )
 }
 
